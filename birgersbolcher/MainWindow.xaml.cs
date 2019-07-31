bddfc94604486061ff73f3464e51ded899b84ede
nkyoko <notorious.droid@gmail.com>
2019-07-31 14:54:53 +0200

    done design working on backend

--- birgersbolcher/MainWindow.xaml.cs
+++ birgersbolcher/MainWindow.xaml.cs
@@ -1,56 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using birgersbolcher.database;
+using MySql.Data.MySqlClient;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace birgersbolcher {
+namespace birgersbolcher
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
+        private MySqlConnection connection = null;
+        MySqlCommand command;
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+
         public MainWindow()
         {
+            DBConnection db = DBConnection.Instance();
+            if (db.IsConnect())
+            {
+                connection = db.Connection;
+            }
+            else
+            {
+                string messageBoxText = "Cannot connect to database, please contact application support";
+                string caption = "Birgers Bolcher";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+            }
+
             InitializeComponent();
+              
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
-        }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            System.Environment.Exit(1);
-
+            Environment.Exit(1);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Minimized;
+            WindowState = WindowState.Minimized;
+        }
+
+    
+        private void LeftMouseDown_Event(object sender, EventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void registerBolcher_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void TabablzControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void register_color_Click(object sender, RoutedEventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "NSERT INTO color (`id`, `name`) VALUES (,)";
+            command.Parameters.Add("@rownum", MySqlDbType.Int32);
+            command.Parameters["@rownum"].Value = -1;
+            adapter.SelectCommand = command;
+
+        }
+
+        private void register_taste_type_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void register_taste_sourness_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void register_taste_strength_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Notred_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void between10_12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void under10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Searchbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Showall_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void heaviestbolcher_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void randombutton_Click(object sender, RoutedEventArgs e)
         {
 
         }
